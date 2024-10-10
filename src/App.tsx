c7d84b4f4d49128a1e0ae6c264e295ae01d5a9ed
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2024-10-10 13:53:52 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react'
-import { SearchPage } from './SearchPage'
-import { LoginPage } from './LoginPage'
+import { BrowserRouter as Router } from 'react-router-dom'
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
-  Container,
   Box,
+  Container,
 } from '@mui/material'
+import { LoginPage } from './LoginPage'
+import { SearchPage } from './SearchPage'
 
 export const App: React.FC = () => {
   const [authenticated, setAuthenticated] = useState<boolean>(false)
   const [username, setUsername] = useState<string | null>(null)
-  const [averageSearchDuration, setAverageSearchDuration] = useState<number>(0)
-  const [totalSearchTime, setTotalSearchTime] = useState<number>(0)
-  const [searchCount, setSearchCount] = useState<number>(0)
+  const [searchDuration, setSearchDuration] = useState<number>(0)
 
   const handleLogin = (user: string) => {
     setUsername(user)
@@ -27,47 +26,35 @@ export const App: React.FC = () => {
     setAuthenticated(false)
   }
 
-  const updateAverageSearchDuration = (duration: number) => {
-    const newTotalSearchTime = totalSearchTime + duration
-    const newSearchCount = searchCount + 1
-    const newAverage = newTotalSearchTime / newSearchCount
-
-    setTotalSearchTime(newTotalSearchTime)
-    setSearchCount(newSearchCount)
-    setAverageSearchDuration(newAverage)
-  }
-
   return (
-    <Container maxWidth="md">
-      {/* AppBar for header with authentication status */}
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            {authenticated ? `Welcome, ${username}!` : 'Book Search App'}
-          </Typography>
-          {authenticated ? (
-            <Button color="inherit" onClick={handleLogout}>
-              Logout
-            </Button>
-          ) : (
-            <Button color="inherit" onClick={() => handleLogin('User')}>
-              Login
-            </Button>
-          )}
-          <Box ml={2}>
-            <Typography variant="body2">
-              Avg Search Duration: {averageSearchDuration.toFixed(2)} ms
+    <Router>
+      <Container maxWidth="md">
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              {authenticated ? `Welcome, ${username}!` : 'Book Search App'}
             </Typography>
-          </Box>
-        </Toolbar>
-      </AppBar>
+            {authenticated && (
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            )}
+            <Box ml={2}>
+              <Typography variant="body2">
+                Avg Search Duration: {searchDuration.toFixed(2)} ms
+              </Typography>
+            </Box>
+          </Toolbar>
+        </AppBar>
 
-      {/* Conditional rendering of SearchPage or LoginPage */}
-      {authenticated ? (
-        <SearchPage setSearchDuration={updateAverageSearchDuration} />
-      ) : (
-        <LoginPage onLogin={handleLogin} />
-      )}
-    </Container>
+        {authenticated ? (
+          <SearchPage setSearchDuration={setSearchDuration} />
+        ) : (
+          <LoginPage onLogin={handleLogin} />
+        )}
+      </Container>
+    </Router>
   )
 }
+
+export default App
